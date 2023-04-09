480afe43942a519b5593cad77e1d67b58f1bf2fd
Animesh  Singh <animeshrathore255@gmail.com>
2023-04-10 00:22:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let dogImageDiv=document.getElementById("dogImageDiv");
+let loadBtn=document.getElementById("loadImage");
+
+let loadDogImage=()=>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response=>response.json())
+    .then(json=>{
+
+        dogImageDiv.innerHTML=`<img src='${json.message}' alt="" height=400 width 400>`
+    });
+}
